cf9232feac798bf91d7a61e308dfa9955d6fe383
Chris Williams <chris@nitron.org>
2011-02-02 13:16:29 -0500

    Fixed an issue raised by realyze on github where CRLF was actually sent as LFCR.

--- cas_provider/views.py
+++ cas_provider/views.py
@@ -59,10 +59,10 @@ def validate(request):
             ticket = ServiceTicket.objects.get(ticket=ticket_string)
             username = ticket.user.username
             ticket.delete()
-            return HttpResponse("yes\n\r%s\n\r" % username)
+            return HttpResponse("yes\r\n%s\r\n" % username)
         except:
             pass
-    return HttpResponse("no\n\r\n\r")
+    return HttpResponse("no\r\n\r\n")
     
 def logout(request, template_name='cas/logout.html'):
     url = request.GET.get('url', None)
