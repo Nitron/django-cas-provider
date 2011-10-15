17b01c928284a4f84a048b50e23c56648e86fe65
Marek Stępniowski <marek@stepniowski.com>
2011-10-15 12:22:53 +0200

    Removed unneeded imports.

--- cas_provider/views.py
+++ cas_provider/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login, logout as auth_logout
 
